b3696138659b711bf1498035a7821daacc326bc6
Atasek <55684330+Atasek@users.noreply.github.com>
2020-09-19 15:18:21 +0300

    Создать базовую страницу index.html

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-var date = new Date();
-var day = date.getDate();
-var month = date.getMonth() + 1;
-var year = date.getFullYear();
 
-if (month < 10) { month = "0" + month; }
-if (day < 10) { day = "0" + day; }
+function setDate() {
+    var date = new Date();
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
 
-const today = year + "-" + month + "-" + day;
-document.getElementById('theDate').value = today;
+    if (month < 10) {
+        month = "0" + month;
+    }
+    if (day < 10) {
+        day = "0" + day;
+    }
+
+    const today = year + "-" + month + "-" + day;
+    document.getElementById('theDate').value = today;
+}
+
+function getFrom(document,element1,element2) {
+    let form = document.forms.form; // <form name="my"> element
+    const elem1 = form.elements.calendar; // <input name="one"> element
+    const elem2 = form.elements.text; // <input name="one"> element
+    alert(elem1.value); // 1
+}
