a7cae607a6260f007040162849327edfe330ce3f
Atasek <55684330+Atasek@users.noreply.github.com>
2020-09-19 12:29:42 +0300

    Создать базовую страницу index.html

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const searchForm = document.search;
-searchForm.submit();
-searchForm.reset();
+var date = new Date();
+var day = date.getDate();
+var month = date.getMonth() + 1;
+var year = date.getFullYear();
+
+if (month < 10) { month = "0" + month; }
+if (day < 10) { day = "0" + day; }
+
+const today = year + "-" + month + "-" + day;
+document.getElementById('theDate').value = today;
